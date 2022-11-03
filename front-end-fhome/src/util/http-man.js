720de9a8aaa5aff507271fb05411a1d7091ac908
alanfvn <alandavidg16@gmail.com>
2022-11-03 02:18:17 -0600

    Fix endpoint back-end

--- front-end-fhome/src/util/http-man.js
+++ front-end-fhome/src/util/http-man.js
@@ -4,7 +4,7 @@ import { get_token } from "./cookie-man";
 const token = get_token()
 
 const HttpMan = axios.create({
-  baseURL: "http://192.168.0.100:3001", 
+  baseURL: "http://localhost:3001", 
   headers: { Authorization: token }
 }) 
 
